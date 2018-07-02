a21b6d731d72f8b1f324b629b7276b706dcebe42
Markus Malkusch <markus@malkusch.de>
2018-07-02 13:46:21 +0200

    Fix double apply
    The recent change with logged in applications resulted in double
    applications. For some reason wg-gesucht requires a request to any URL
    after a login.

--- src/main/java/de/malkusch/getroom/infrastructure/LoginAuthenticationService.java
+++ src/main/java/de/malkusch/getroom/infrastructure/LoginAuthenticationService.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -19,11 +21,12 @@ final class LoginAuthenticationService implements AuthenticationService {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginAuthenticationService.class);
 
     LoginAuthenticationService(@Value("${login.username}") String username, @Value("${login.password}") String password,
-            @Value("${login.url}") String url, RestTemplate rest) throws IOException {
+            @Value("${login.url}") String api, @Value("${homeUrl}") String home, RestTemplate rest) throws IOException {
 
         this.username = username;
         this.password = password;
-        this.url = url;
+        this.api = api;
+        this.home = home;
         this.rest = rest;
 
         authenticate();
@@ -41,7 +44,8 @@ final class LoginAuthenticationService implements AuthenticationService {
         authenticate();
     }
 
-    private final String url;
+    private final String api;
+    private final String home;
     private final String username;
     private final String password;
     private final RestTemplate rest;
@@ -59,8 +63,13 @@ final class LoginAuthenticationService implements AuthenticationService {
 
         LOGGER.debug("Login {}", username);
         try {
-            ResponseEntity<?> response = rest.postForEntity(url, new HttpEntity<>(login), null);
+            ResponseEntity<?> response = rest.postForEntity(api, new HttpEntity<>(login), null);
             cookies = new AuthenticationCookies(HttpCookie.parse(response.getHeaders().getFirst("Set-Cookie")));
+
+            // For some reasone this is necessary after a login
+            HttpHeaders headers = new HttpHeaders();
+            headers.set(HttpHeaders.COOKIE, cookies.toString());
+            rest.exchange(home, HttpMethod.GET, new HttpEntity<>(headers), String.class);
 
         } catch (RestClientException e) {
             throw new IOException(String.format("Could not login %s", username), e);
